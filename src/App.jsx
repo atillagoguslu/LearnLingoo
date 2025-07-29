87a10dc687b72aef3b73512fa7acfb20347c439c
Atilla Göğüslü <atillagoguslu@hotmail.com>
2025-07-29 23:16:10 +0300

    App fixed

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,11 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return <>
+  <div>
+    <h1>Hello World</h1>
+  </div>
+  </>;
 }
 
 export default App;
